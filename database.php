fe875c687307278ec18ec550044b2f8c1b52e73c
Jose Salinas <salinasj14@students.ecu.edu>
2017-02-14 21:11:47 -0500

    Add files via upload

--- database.php
+++ database.php
@@ -1,17 +1,15 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Jose Salinas, Earl
+ * User: Koobi Eyota, Jose Salinas, Earl Blizzard
  * Date: 2/6/17
  * Time: 6:09 PM
  */
-
 //Connection to DB
 $server = "tcp:gaminggroup.database.windows.net,1433";
 $connectionTimeoutSeconds = 30;
 $connectionOptions = array("Database"=>"Game", "Uid"=>"salinasj14", "PWD"=>"Eastcarolina14", "LoginTimeout" => $connectionTimeoutSeconds);
 $conn = sqlsrv_connect($server,$connectionOptions);
-
 //Strings to access from client side
 $name = $_GET['name'];
 $kills = $_GET['kills'];
@@ -19,7 +17,6 @@ $deaths = $_GET['deaths'];
 $score = $_GET['score'];
 $team = $_GET['team'];
 $tableOperation = $_GET['operation'];
-
 //testing to see if DB is connected
 if($conn != true)
 {
@@ -27,17 +24,13 @@ if($conn != true)
 }
 //else
 //{
-  //  echo "connected to my DB";
-   // echo "<br>";
+//  echo "connected to my DB";
+// echo "<br>";
 //}
-
 //creating the table
-
-//if($tableOperation == 'create')
-//{
-    //echo "you have called table operation and create";
-    //echo "<br>";
-    $createCmd = "CREATE TABLE [dbo].[leaderboards]
+if($tableOperation == 'create')
+{
+$createCmd = "CREATE TABLE [dbo].[leaderboards]
     (
 	  [Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
       [Name] VARCHAR(50) NOT NULL, 
@@ -46,79 +39,33 @@ if($conn != true)
       [Scores] INT NOT NULL, 
       [Team] INT
     )";
-    $create = sqlsrv_query($conn, $createCmd);
-   //echo "you have finished calling table operation (create)";
-//}
-
-// Koobi's code'
-/*
-$stmt = "select * from [dbo].[leaderboards]";
-$result = sqlsrv_query($conn, $stmt);
-while($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC))
-{
-    //print_r($row);
-    //echo"<br />";
-    //print "<tr>\n";
-    echo "<br>";
-    echo $row['Name'].", ".$row['Kills'].", ".$row['Deaths'].", ".$row['Scores'].", ".$row['Team']."<br />";
-    echo "<br>";
-}
-*/
-
-// My code
-
-$tsql = "SELECT * FROM leaderboards";
-$getProducts = sqlsrv_query($conn, $tsql);
-if ($getProducts == FALSE)
-{
-    die(FormatErrors(sqlsrv_errors()));
-}
-$productCount = 0;
-$ctr = 0;
-$counter = 0;
-/*
-    echo "<br>";
-    echo "Id Name Kills Deaths Scores Team ";
-    "<br />";
-    echo "<br>";
-*/
-while( $row = sqlsrv_fetch_array( $getProducts, SQLSRV_FETCH_ASSOC ))
-{
-    echo $row['Name']."|".$row['Kills']."|".$row['Deaths']."|".$row['Scores']."|".$row['Team'].";";
+$create = sqlsrv_query($conn, $createCmd);
 }
 
+if($tableOperation == "showData")
+{
+    $tsql = "SELECT * FROM leaderboards";
+    $getProducts = sqlsrv_query($conn, $tsql);
+    if ($getProducts == FALSE)
+    {
+        die(FormatErrors(sqlsrv_errors()));
+    }
 
-/*
-// Code for Earl
-$tsql = "SELECT * FROM leaderboards";
-$getProducts = sqlsrv_query($conn, $tsql);
-if ($getProducts == FALSE)
-{
-    die(FormatErrors(sqlsrv_errors()));
+    while( $row = sqlsrv_fetch_array( $getProducts, SQLSRV_FETCH_ASSOC ))
+    {
+        echo $row['Name']."|".$row['Kills']."|".$row['Deaths']."|".$row['Scores']."|".$row['Team'].";";
+    }
 }
-$productCount = 0;
-$ctr = 0;
-$counter = 0;
-while( $row = sqlsrv_fetch_array( $getProducts, SQLSRV_FETCH_ASSOC ))
-{
-    print_r($row);
-}
-sqlsrv_free_stmt($getProducts);
-*/
 
 //inserting values
 if($tableOperation == "makePlayer")
 {
-    echo "you have called table operation (makePlayer)";
     //it should auto increment and have a null value for team.
-    $makeCmd = "INSERT into [dbo].[leaderboards] values ('$name',0,0,0,null)";
+    $makeCmd = "INSERT into [dbo].[leaderboards] values ('$name',0,0,0,0)";
     $makePlayer = sqlsrv_query($conn, $makeCmd);
-    echo "you have finished calling table operation (makePlayer) \n";
-    echo "name is $name";
 }
 
 //removing a player from the database
-
 if($tableOperation == "deletePlayer")
 {
     echo "about to check delete player";
@@ -128,7 +75,6 @@ if($tableOperation == "deletePlayer")
     $deletePlayer = sqlsrv_query($conn, $deletePlayerCmd);
     echo "you have finished calling table operation (deletePlayer) \n";
 }
-
 //update the kill in the table
 if($tableOperation == "updateKill")
 {
@@ -137,7 +83,6 @@ if($tableOperation == "updateKill")
     $updateKill = sqlsrv_query($conn,$killCmd);
     echo "you have finished calling  table operation (updatingKill)";
 }
-
 //updating the death in the table
 if($tableOperation == "updateDeath")
 {
@@ -146,7 +91,6 @@ if($tableOperation == "updateDeath")
     $updateDeath = sqlsrv_query($conn,$deathCmd);
     echo "you have finished calling  table operation (updatingDeath)";
 }
-
 //incrementing the score
 if($tableOperation == "incScores")
 {
@@ -155,7 +99,6 @@ if($tableOperation == "incScores")
     $incScores = sqlsrv_query($conn,$incCmd);
     echo "you have finished calling  table operation (incScores)";
 }
-
 //setting teams
 if($tableOperation == "setTeam")
 {
@@ -177,7 +120,6 @@ if($tableOperation == "setTeam")
     echo "you have finished calling  table operation (setTeam)";
     echo "<br>";
 }
-
 //delete table
 if($tableOperation == "deleteTable")
 {
@@ -186,6 +128,3 @@ if($tableOperation == "deleteTable")
     $delete = sqlsrv_query($conn,$deleteCmd);
     echo "you have finished calling table operation (delete)";
 }
-
-
-
