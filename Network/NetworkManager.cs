3870e8bc33d33d7ce48a7498608761be4f6e224c
Christen Mills <Christenmills@outlook.com>
2017-02-25 12:17:02 -0500

    Add files via upload
    Run Build.exe twice in game room LEVELZERO, and another player should be made inside the game room
    
    NetworkManager: takes care of connection
    NetworkPlayer: attach to prefab then place prefab in root of Resources folder

---
+++ Network/NetworkManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour {
+    const string VERSION = "v0.1";
+    public string roomName = "LevelZero";
+    public string playerPrefabName = "PlayerOne";
+    public Transform spawnPoint;
+
+	// Use this for initialization
+	void Start () {
+        PhotonNetwork.ConnectUsingSettings(VERSION);
+    }
+
+    void OnJoinedLobby()
+    {
+        RoomOptions roomOptions = new RoomOptions() { isVisible = false, maxPlayers = 4 };
+        PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, TypedLobby.Default);
+    }
+
+    void OnJoinedRoom() {
+        PhotonNetwork.Instantiate(playerPrefabName,
+                                    spawnPoint.position,
+                                    spawnPoint.rotation,
+                                    0);
+    }
+
+}
---
+++ Network/NetworkPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkPlayer : MonoBehaviour {
+    public GameObject myCamera;
+	// Use this for initialization
+	void Start () {
+
+    }
+
+}
