4e681ca8f84251c8ca575458cfd6381b4dc013b6
ablanchard95 <blancharda16@students.ecu.edu>
2017-03-13 21:41:27 -0400

    Add files via upload
    Allows for easy weapon spawns

---
+++ WeaponSpawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSpawn : MonoBehaviour {
+
+	public Transform[] spawnLocations;
+	public GameObject[] weaponToSpawnPrefab;
+	public GameObject[] weaponToSpawnClone;
+
+	void Start(){  //Aaron B, script allows for easy weapon spawning of a variety of weapons
+		spawnWeapon ();
+	}
+
+	void spawnWeapon(){
+		weaponToSpawnClone[0]=Instantiate(weaponToSpawnPrefab[0], spawnLocations[0].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
+	}
+}
